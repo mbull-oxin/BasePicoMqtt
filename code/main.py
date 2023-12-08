79a639b5ac4acd5e7857dd2b641d535c427ec822
Matthew Bull <matt@digitao.org>
2023-12-08 13:31:40 +0000

    debugged versions of main + added initial version of power monitor and example dc's

--- code/main.py
+++ code/main.py
@@ -1,126 +1,197 @@
 # main.py
 from _thread import allocate_lock,start_new_thread
 import umqttsimple as mqtt
-import pyb,array
+import machine,array,time,micropython,json
+
+micropython.alloc_emergency_exception_buf(200)
+
 USE_DC=[]
 
 DISCON=0
 CONN=1
 ERR=2
 
+class RunFlag:
+    def __init__(self,flag=True):
+        self._flag=flag
+    def set(self,state):
+        self._flag=state
+    def __bool__(self):
+        if self._flag:
+            return True
+        else:
+            return False
+
 class MqttClient:
     def __init__(self,conf):
-        self.client=mqtt.MQTTClient(conf['id'],conf['server'],conf['port'])
-        self.run=True
+        print(conf)
+        self.client=mqtt.MQTTClient(conf['id'],conf['server'],int(conf['port']))
+        self._run=None
         self.con_stat=DISCON
         self.machine_id=conf['id']
-    def run(self,d_s):
-        while self.run:
-            msg=self.queue.get(timeout=20)
-            if not msg:
+        try:
+            print(self.client.port)
+            self.client.connect(clean_session=True)
+            self.con_stat=CONN
+            print('[MqttClient] contected')
+        except Exception as exc:
+            print('[MqttClient] error connecting to broker',exc)
+            self.con_stat=ERR
+            #continue
+    def run(self,d_s,run_flag):
+        self._run=run_flag
+        while self._run:
+            #print('getting event')
+            msg=d_s.get(timeout=20)
+            #print('[MqttClient] got',msg)
+            if not msg and self.con_stat==CONN:
+                print('ping')
                 self.client.ping()
                 continue
-            if self.conn_stat!=CONN:
+            if msg and self.con_stat!=CONN:
                 try:
+                    print(self.client.port)
                     self.client.connect(clean_session=True)
                     self.con_stat=CONN
-                except:
+                    print('[MqttClient] contected')
+                except Exception as exc:
+                    print('[MqttClient] error connecting to broker',exc)
                     self.con_stat=ERR
-            print('sending',msg)
-            self.client.publish('%s/%s' % (self.machine_id,msg[0]),msg[1])
+                    continue
+            if msg:
+                print('[MqttClient] sending',msg)
+                try:
+                    self.client.publish('%s/%s' % (self.machine_id,msg[0]),msg[1])
+                except Exception as exc:
+                    print('[MqttClient] error publishing to broker',exc)
+                    self.con_stat=ERR
+                    continue
+        self._run.set(False)
 
-class Queue(list):
+class Queue():
     def __init__(self):
-        list.__init__(self,[])
+        self._list=[]
         self._mutex=allocate_lock()
         self._event=allocate_lock()
         self._event.acquire()
-    def push(self,obj):
+        self._timer=machine.Timer()
+    def put(self,obj):
         with self._mutex:
-            self.append(obj)
+            self._list.append(obj)
             self._event.release()
-        self._event.acquire()
+        time.sleep(.1)
+        self._event.acquire(False)
+    def _timeout(self,timer):
+        print('timeout',timer,self._timer)
+        if timer==self._timer:
+            self._event.release()
+            micropython.schedule(self._reacquire,[])
+    def _reacquire(self,*args):
+            while 1:
+                if self._event.acquire(False):
+                    break
     def get(self,timeout=None):
-        gotit=self._event.acquire(True,timeout)
-        if gotit:
-            self._event.release()
+        if timeout:
+            self._timer.init(mode=machine.Timer.ONE_SHOT,period=timeout*1000,callback=self._timeout)
+        print('entering loop with timeout',timeout*1000)
+        while 1:
+            #print('entering loop with timeout',timeout*1000)
+            gotit=self._event.acquire(False)
+            #print(gotit)
+            if gotit:
+                self._event.release()
+                time.sleep(1)
+                break
+            else:
+                time.sleep(0.1)
+        if len(self._list):
             with self._mutex:
-                evt=this.pop(0)
-            self._event.acquire()
+                evt=self._list.pop(0)
             return evt
         else:
             return None
 
-class Scheduler:
+class DcManager:
     def __init__(self,queue):
         self._queue=queue
         self.dc_modules=[]
-        self.samp_freq=0
-        self.req_samp_rates={}
-        self._multipliers={}
-        self.readings=array.array('H')
-    def add(self,module):
-        if SAMPLE_RATE,dc not in module:
-            raise ImportError('module is not a dc module')
-        if module.SAMPLE_RATE!=self.sample_freq:
-            self.req_sample_rates[module.__name__]=module.SAMPLE_RATE
-            self.samp_freq=max(self.samp_freq,module.SAMPLE_RATE)
-            self.recalc_mults()
+        self.dc_obj=[]
+        self.output=[]
+        self._run=None
+    def add(self,module,conf):
         self.dc_modules.append(module)
-    def recalc_mults(self):
-        self._multipliers={}
-        for mod in self.req_samp_rates:
-            n_mult=self.samp_freq/self.req_samp_rates[mod]
-            if n_mult in self._multipliers:
-                self._multipliers[n_mult].append(mod)
-            else:
-                self._multipliers[n_mult]=[mod]
-    def run(self):
-        self._timer=machine.Timer(id='dc_sched')
-        self._timer.init(freq=self.samp_freq,mode=machine.Timer.PERIODIC,callback=self.isr)
-        while 1:
-            if len(self.readings)>0:
-                int_state=pyb.disable_interrupts()
-                while len(self.readings):
-                    self.send(self.readings[curr_reading])
-                self.readings_index=0
-                pyb.enable_interrupts(int_state)
-            else:
-                time.sleep(.1)
-    def isr(self,)
+        try:
+           dc=module.dc(conf)
+        except AttributeError:
+            print('module is not a dc module')
+            raise ImportError('module is not a dc module')
+        self.dc_obj.append(dc)
+        dc.run(_run)
+    def run(self,run_flag):
+        self._run=run_flag
+        while self._run:
+            try:
+                for mod_idx in range(len(self.dc_modules)):
+                    mod_name=self.dc_modules[mod_idx].dc_name
+                    reading=self.dc_obj[mod_idx].getReading()
+                    self.send(mod_name,reading)
+                time.sleep(1)
+            except KeyboardInterrupt:
+                self._run.set(False)
+                break
+    def send(self,mod_name,reading):
+        print(mod_name,reading)
+        self._queue.put((mod_name,json.dumps({'reading':reading,'timestamp':time.time()})))
 
-
-
-if __name__=='__main__':
-    import network
-    config={}
-    _curr_section=None
-    c_f=open('node.conf','r')
-    for c_l in c_f.readlines():
-        if '[' in c_l:
-            _curr_section=c_l[c_l.index('[')+1:c_l.index(']')]
-            config[_curr_section]={}
-        elif '=' in c_l:
-            key,val=c_l.strip().split('=',1)
-            if _curr_section:
-                config[_curr_sction][key]=val
-            else:
-                config[key]=val
-    c_f.close()
-    net_conf=config.pop('network',None)
-    if net_conf:
-        nic=network.WLAN(network.STA_IF)
+import network
+config={}
+_run=RunFlag()
+_curr_section=None
+c_f=open('node.conf','r')
+for c_l in c_f.readlines():
+    #print(c_l)
+    if '#' in c_l:
+        continue
+    if '[' in c_l:
+        _curr_section=c_l[c_l.index('[')+1:c_l.index(']')]
+        config[_curr_section]={}
+    elif '=' in c_l:
+        key,val=c_l.strip().split('=',1)
+        if _curr_section:
+            config[_curr_section][key]=val
+        else:
+            config[key]=val
+c_f.close()
+print(config['network'])
+net_conf=config.pop('network',None)
+nic=None
+if net_conf:
+    nic=network.WLAN(network.STA_IF)
+    nic.active(True)
+    nic.connect(net_conf['ssid'],net_conf['key'])
+    while nic.status()!=network.STAT_GOT_IP:
+        if nic.status==network.STAT_CONNECT_FAIL:
+            nic.active(False)
+            print('[network] Connection failed')
+        elif nic.status==network.STAT_NO_AP_FOUND:
+            print('[network] No AP found')
+        elif nic.status==network.STAT_WRONG_PASSWORD:
+            print('[network] Incorrect password')
+        else:
+            print('network status',nic.status())
+        time.sleep(1)
         nic.active(True)
-        nic.connect(config['network']['ssid'],config['network']['key'])
-    queue=Queue()
-    cli=MqttClient(config.pop('mqtt'))
-    sched=Scheduler(queue)
-    for key in config:
-        # configure _dc module in scheduler
-        if 'module' in config[key]:
-            exec('import '+config[key]['module'])
-            ds_m=eval(config[key]['module'])
-            sched.add(ds_m)
-    sched.run()
-    cli.run()
+time.sleep(1)
+queue=Queue()
+cli=MqttClient(config.pop('mqtt'))
+mgr=DcManager(queue)
+for key in config:
+    # configure _dc module in scheduler
+    if 'module' in config[key]:
+        exec('import '+config[key]['module'])
+        ds_m=eval(config[key]['module'])
+        mgr.add(ds_m,config[key])
+#start_new_thread(mgr.run,(),{})
+start_new_thread(cli.run,(queue,_run),{})
+mgr.run(_run)
     
